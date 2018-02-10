28e1ce7b0efa474c8ce9cc70d558fafd940dc4a7
Lisa Lee <snickers495@gmail.com>
2018-02-10 20:30:30 +0000

    automatically backed up by learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default class QuoteCard extends React.Component {
   render() {
-  return(
-    <form>
-    <label>Quote</label>
-    <input type="text" name="quote"/>
-    <label>Author</label>
-    <input type="text" name="author"/>
-    <input type="submit"/>
-    </form>
-  )
-  
+    return(
+      <form>
+      <label>Quote</label>
+      <input type="text" name="quote"/>
+      <label>Author</label>
+      <input type="text" name="author"/>
+      <input type="submit"/>
+      </form>
+    )
+  }
 }
