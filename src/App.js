067d994b8f5c7251711af54e3f6cdb29dedcb7d5
Lisa Lee <snickers495@gmail.com>
2018-02-10 20:29:47 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import QuoteForm from './components/QuoteForm'
 
-export class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="container-fluid">
