9a9e320974e3183a75f4c89232ca4c8ef82171d3
Lisa Lee <snickers495@gmail.com>
2018-02-10 20:31:20 +0000

    automatically backed up by learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class QuoteCard extends React.Component {
+export default class QuoteForm extends React.Component {
   constructor(){
     super()
     this.state = {
