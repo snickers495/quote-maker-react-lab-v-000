771b082a6b8b66d327784e3b9ada46be7dea74bd
Lisa Lee <snickers495@gmail.com>
2018-02-10 20:28:13 +0000

    automatically backed up by learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 const QuoteCard = (props) => {
   return(
-    
+    <form>
+    <label>Quote</label>
+    <input type="text" name="quote"/>
+    <label>Author</label>
+    <input type="text" name="author"/>
+    <input type="submit"/>
+    </form>
   )
 }
